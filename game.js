4da215ce63965424e208cec7efdaf68d2959094c
Vasak Aramian <yooopiav@gmail.com>
2021-02-17 08:49:25 +0500

    hw2 30%

---
+++ game.js
@@ -0,0 +1,47 @@
+// const minimist = require("minimist");
+
+// let arguments = minimist(process.argv.slice(2), {
+//   alias: {
+//     help: "h",
+//     test: "t",
+//   },
+//   boolean: ["lol"],
+// });
+
+// console.log(arguments);
+const Sugar = require("sugar");
+const shapes = ["rock", "paper", "scissor"];
+
+let userShape = "rock";
+let botShape = "scissor";
+
+const duel = (user, bot) => {
+  if (user === bot) {
+    console.log("Draw!");
+    return "Draw";
+  }
+
+  switch (user) {
+    case "rock":
+      bot === "paper" ? console.log("User lost :(") : console.log("User win!");
+      break;
+
+    case "paper":
+      bot === "scissor"
+        ? console.log("User lost :(")
+        : console.log("User win!");
+      break;
+
+    case "scissor":
+      bot === "rock" ? console.log("User lost :(") : console.log("User win!");
+      break;
+  }
+};
+
+const game = () => {
+  let bot = Sugar.Array.sample(shapes);
+  let user = Sugar.Array.sample(shapes);
+  duel(user, bot);
+};
+
+game();
