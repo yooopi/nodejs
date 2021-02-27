d08a5d9c99f45363dc0bfeb15e69d5cd812f664c
Vasak Aramian <yooopiav@gmail.com>
2021-02-27 20:33:07 +0500

    hw3hb

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const handlebars = require("handlebars");
+const templating = require("consolidate");
 const translate = require("./translate");
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine("hbs", templating.handlebars);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "/views"));
+
 app.post("/translate", async (req, res) => {
   const yres = await translate(req.body.texts, req.body.targetLanguageCode);
-
-  // Такой шаблон не лучший вариант, но быстрый
-  res.send(`<form method="POST" action="/translate">
-  <input name="texts" type="text" />
-  <select name="targetLanguageCode">
-    <option value="en">en</option>
-    <option value="it">it</option>
-    <option value="es">es</option>
-    <option value="de">de</option>
-    <option value="fr">fr</option>
-  </select>
-  <button type="submit">SEND</button>
-</form>
-<p>Target language was: ${req.body.targetLanguageCode}</p>
-<h3>${yres.data.translations[0].text}</h3>
-<script></script>`);
+  res.render("index.hbs", { translation: yres.data.translations[0].text });
 });
 
 app.listen(8080, () => console.log("Listening on port https://localhost:8080"));
