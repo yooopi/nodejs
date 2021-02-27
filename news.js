61a8120938fee708109b234e81a6d8857055f8ec
Vasak Aramian <yooopiav@gmail.com>
2021-02-27 21:55:37 +0500

    hw4 5%

--- news.js
+++ news.js
@@ -1,13 +1,34 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-const newsSelector = `.post__title_link`;
 
-axios
-  .get("https://habr.com/ru/news/")
-  .then((res) => {
-    const $ = cheerio.load(res.data);
-    $(newsSelector).each(function (i, elem) {
-      console.log($(this).text());
-    });
-  })
-  .catch((err) => console.log(`Error: ${err}`));
+const data = {
+  habr: {
+    url: "https://habr.com/ru/news/",
+    selector: ".post__title_link",
+  },
+  dddnews: {
+    url: "https://3dnews.ru/",
+    selector:
+      "#allnews > div.allnews-col.rncol > div.content-block-data.white > ul > li > a",
+  },
+  navalny: {
+    url: "https://navalny.com/",
+    selector:
+      "body > div.b-page.b-layout > div > div > div > div.l-grid__item.nine-twelfths > div > div > div > h2 > a",
+  },
+};
+
+module.exports = async (resource) => {
+  const res = await axios
+    .get(data[resource].url)
+    .then((res) => {
+      const news = [];
+      const $ = cheerio.load(res.data);
+      $(data[resource].selector).each(function (i, elem) {
+        news.push($(this).text());
+      });
+      return news;
+    })
+    .catch((err) => console.log(`Error: ${err}`));
+  return res;
+};
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const handlebars = require("handlebars");
 const templating = require("consolidate");
-const translate = require("./translate");
+const news = require("./news");
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
@@ -13,9 +13,14 @@ app.engine("hbs", templating.handlebars);
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.post("/translate", async (req, res) => {
-  const yres = await translate(req.body.texts, req.body.targetLanguageCode);
-  res.render("index.hbs", { translation: yres.data.translations[0].text });
+app.get("/", (req, res) => {
+  res.render("index.hbs");
+});
+
+app.post("/news", async (req, res) => {
+  console.log(req.body.resource);
+  const reqNews = await news(req.body.resource);
+  res.render("index.hbs", { reqNews: reqNews });
 });
 
 app.listen(8080, () => console.log("Listening on port https://localhost:8080"));
